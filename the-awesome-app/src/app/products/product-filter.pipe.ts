3ba2ce9fc486838244bef8d44dff066aca771470
ChunWei Lok <cwlok@motorolasolutions.com>
2023-08-15 14:59:26 +0800

    add pipe

--- the-awesome-app/src/app/products/product-filter.pipe.ts
+++ the-awesome-app/src/app/products/product-filter.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../model/product';
 
 @Pipe({
   name: 'productFilter'
 })
 export class ProductFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(input: Array<Product>, searchKey: string): Array<Product> {
+    if (!searchKey) {
+      return input;
+    } else {
+      return input.filter(function(product, index) {
+        return product.id?.toString() === searchKey ||
+                product.name?.toLowerCase().includes(searchKey.toLowerCase()) ||
+                product.price?.toString() === (searchKey) ||
+                product.description?.toLowerCase().includes(searchKey.toLowerCase())
+      })
+    }
   }
-
 }
